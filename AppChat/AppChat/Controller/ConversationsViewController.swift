dc8edee0fbcf0ef83ddbcf8831f46378a7034fa7
taikhoan01 <67822370+taikhoan01@users.noreply.github.com>
2020-11-15 13:38:37 +0700

    update

---
+++ AppChat/AppChat/Controller/ConversationsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  AppChat
+//
+//  Created by Apple on 10/23/20.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+         
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false, completion: nil)
+            
+        }
+    }
+
+
+}
+
